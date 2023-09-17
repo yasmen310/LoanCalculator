18fe5df8e8102c36ede137f2f78d693f7048cf96
Yasmen Ahmed <130899829+yasmen310@users.noreply.github.com>
2023-09-17 14:55:30 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+function calculateLoan(){
+  const loanAmount=parseFloat(document.getElementById("loanAmountInput").value);
+  const interestRate=parseFloat(document.getElementById("interestRateInput").value);
+  const loanTerm=parseFloat(document.getElementById("loanTermInput").value);
+
+  if(isNaN(loanAmount) ||isNaN(interestRate) ||isNaN(loanTerm)){
+    showError("Please Enter Valid Number For All Fields");
+    return;   
+  }
+  const monthlyInterest=interestRate / 100 /12;
+  const totalPayments=loanTerm;
+  const monthlyPayment=(loanAmount * monthlyInterest)/(1-Math.pow(1+monthlyInterest,-totalPayments));
+ 
+  const totalInterest=(monthlyPayment*totalPayments)-loanAmount;
+   displayResult(monthlyPayment,totalInterest);
+}
+
+function displayResult(monthlyPayment,totalInterest){
+  const resultDiv=document.getElementById("result");
+
+  resultDiv.innerHTML=`
+  <p><strong>Monthly Payment : ${monthlyPayment.toFixed(2)} </strong></p>
+  <p><strong>Total Interest : ${totalInterest.toFixed(2)} </strong></p>
+  
+  `;
+
+}
+function showError(message){
+  const resultDiv=document.getElementById("result");
+  resultDiv.innerHTML=`<p class="error"> ${message}</p>`;
+}
+
+document.getElementById("calculateBtn").addEventListener("click",calculateLoan);
